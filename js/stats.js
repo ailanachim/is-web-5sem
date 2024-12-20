c5553a61db332d2b5a034001ba7835b58cc8d0b0
Ailana <ailanachim@gmail.com>
2024-12-20 12:03:34 +0300

    lab 4

---
+++ js/stats.js
@@ -0,0 +1,36 @@
+(function () {
+    window.addEventListener('load', function () {
+        const loadTime = performance.now();
+        const statsContainer = document.getElementById('loadstats');
+        if (statsContainer) {
+            statsContainer.textContent = `Page loaded for ${loadTime.toFixed(2)} ms.`;
+        }
+    });
+
+    const navLinks = document.getElementsByClassName("menu__link");
+    const currentPage = document.location.pathname.split('/').pop();
+
+    for (let i = 0; i < navLinks.length; i++) {
+        if (navLinks[i].getAttribute('href') === currentPage) {
+            navLinks[i].classList.add('menu__link__active');
+            break;
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const menuToggle = document.getElementById('menu__toggle');
+        const menu = document.querySelector('.menu__list');
+
+        menuToggle.addEventListener('click', () => {
+            console.log('menuToggl clicked');
+            console.log(menu.style.display);
+            if (menu.style.display === "none") {
+                menu.style.display = "flex";
+            } else {
+                menu.style.display = "none";
+            }
+        });
+    });
+
+
+})();
